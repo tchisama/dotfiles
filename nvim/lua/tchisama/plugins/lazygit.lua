e91acbe6439c2d1572b2c07a42d43a9d21c3a292
tchisama <pro.tchisama@gmail.com>
2025-02-23 00:02:49 +0000

    link it with my local laptop config

--- nvim/lua/tchisama/plugins/lazygit.lua
+++ nvim/lua/tchisama/plugins/lazygit.lua
@@ -3,19 +3,19 @@ return {
 	"kdheepak/lazygit.nvim",
 	lazy = true,
 	cmd = {
-		"lazygit",
-		"lazygitconfig",
-		"lazygitcurrentfile",
-		"lazygitfilter",
-		"lazygitfiltercurrentfile",
+		"LazyGit",
+		"LazyGitConfig",
+		"LazyGitCurrentFile",
+		"LazyGitFilter",
+		"LazyGitFilterCurrentFile",
 	},
 	-- optional for floating window border decoration
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 	},
-	-- setting the keybinding for lazygit with 'keys' is recommended in
+	-- setting the keybinding for LazyGit with 'keys' is recommended in
 	-- order to load the plugin when the command is run for the first time
 	keys = {
-		{ "<leader>lg", "<cmd>lazygit<cr>", desc = "lazygit" },
+		{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
 	},
 }
