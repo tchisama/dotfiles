a8f28ad2355023385d9a1660e4c85d6991f9b39b
tchisama <pro.tchisama@gmail.com>
2025-02-26 17:16:59 +0000

    all good

---
+++ nvim/lua/tchisama/plugins/vim-ai.lua
@@ -0,0 +1,3 @@
+return {
+	"madox2/vim-ai",
+}
--- nvim/test.js
+++ nvim/test.js
@@ -1,9 +1,34 @@
-function hello() {
-  console.log("Hello World");
-}
-hello();
+const assert = require("assert");
 
-function test() {
-  console.log("Test");
+function GivethefirstNumberThatIsGreaterThan10(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > 10) {
+      return array[i];
+    }
+  }
 }
-test();
+
+// testing
+
+describe("GivethefirstNumberThatIsGreaterThan10", function () {
+  it("should return the first number greater than 10", function () {
+    assert.strictEqual(
+      GivethefirstNumberThatIsGreaterThan10([1, 5, 12, 8]),
+      12,
+    );
+  });
+
+  it("should return undefined if no number is greater than 10", function () {
+    assert.strictEqual(
+      GivethefirstNumberThatIsGreaterThan10([1, 5, 8]),
+      undefined,
+    );
+  });
+
+  it("should return the first number greater than 10 even if there are multiple", function () {
+    assert.strictEqual(
+      GivethefirstNumberThatIsGreaterThan10([1, 15, 12, 8]),
+      15,
+    );
+  });
+});
