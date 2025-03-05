aa4c2e78eecfe46490faebb98256727062576c6b
tchisama <pro.tchisama@gmail.com>
2025-03-05 16:18:08 +0000

    a

--- nvim/lua/tchisama/plugins/markdown.lua
+++ nvim/lua/tchisama/plugins/markdown.lua
@@ -1,18 +1,24 @@
 return {
 	"MeanderingProgrammer/render-markdown.nvim",
-	opts = {
-		code = {
-			sign = false,
-			width = "block",
-			right_pad = 1,
-		},
-		heading = {
-			sign = false,
-			icons = {},
-		},
-		checkbox = {
-			enabled = false,
-		},
-	},
-	ft = { "markdown", "norg", "rmd", "org", "codecompanion" },
+	dependencies = { "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+	---@module 'render-markdown'
+	---@type render.md.UserConfig
+	opts = {},
+	config = function()
+		require("render-markdown").setup({
+			heading = {
+				sign = true,
+				position = "inline",
+				border = true,
+			},
+			pipe_table = { preset = "round" },
+			sign = {
+				enabled = true,
+				highlight = "RenderMarkdownSign",
+			},
+			indent = { enabled = true },
+		})
+	end,
 }
